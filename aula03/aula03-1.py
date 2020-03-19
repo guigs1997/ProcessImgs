b7a5677b1318f9be96c748155a9d71e0e75ba43d
guigui <guilherme.gasp@hotmail.com>
2020-03-19 19:43:14 -0300

    fecha se eu apertar q varias vezes no final

--- aula03/aula03-1.py
+++ aula03/aula03-1.py
@@ -10,7 +10,7 @@ dim = (512,512)
 watermark = cv2.imread("data/logo.png",cv2.IMREAD_UNCHANGED)
 lH,lW = watermark.shape[:2]
 
-def cutname(string):
+def cutname(string): #método para captar apenas o nome da imagem 
     start = 0
     end = 0 
     end = string.find("\\") 
@@ -24,40 +24,51 @@ for img in glob.glob("data/*.jpg"):
     ovr = np.zeros((oH,oW,4), dtype="uint8")
     ovr[oH - lH - 60:oH - 60, oW - lW - 10:oW - 10] = watermark
     final = n.copy()
-    n = cv2.addWeighted(ovr,0.3,final,1.0,0,final)
-    n = cv2.copyMakeBorder(n,40,10,10,10,cv2.BORDER_CONSTANT,value=[255,255,255]) 
-    n = cv2.resize(n,dim)   
-    n = cv2.putText(n,cutname(img),posicao, 1, 2, 0)
+    n = cv2.addWeighted(ovr,0.3,final,1.0,0,final) #adiciona marca d'água
+    n = cv2.copyMakeBorder(n,40,10,10,10,cv2.BORDER_CONSTANT,value=[255,255,255]) #adiciona borda 
+    n = cv2.resize(n,dim) #resize na imagem pra todas terem o mesmo tamnho
+    n = cv2.putText(n,cutname(img),posicao, 1, 2, 0) #coloca o nome da imagem na borda
     images.append(n)
 
 ind  = 0
 i=1.0
 j=0.0
-while True: 
+ok=True
+
+while ok: 
+    if cv2.waitKey() == 0x51: #fecha janela ao pressionar Q
+        ok=False     
+    if cv2.waitKey() == 0x71: #fecha janela ao pressionar q
+        ok=False     
+
+
     n = images[ind]
-    cv2.imshow('Img',n)      
-    cv2.waitKey(5000)
-    if(ind<3): 
-        for contador in range(10):           
-            i-=0.10
-            j+=0.10
-            n = cv2.addWeighted(images[ind],i,images[ind+1],j,1)
-            cv2.imshow('Img',n)
-            cv2.waitKey(100)   
-        ind+=1
-        i=1.0
-        j=0.0
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break 
-    if cv2.waitKey(1) & 0xFF == ord('Q'):
-        break          
+    cv2.imshow('Img',n)
 
-cv2.waitKey()
+    for repete in range(3):
+        cv2.waitKey(5000) #espera por 5 segundos
+        if(ind<3): 
+            for contador in range(10):           
+                i-=0.10
+                j+=0.10
+                n = cv2.addWeighted(images[ind],i,images[ind+1],j,1) #aplica transição
+                cv2.imshow('Img',n)
+                cv2.waitKey(100)   
+            ind+=1
+            i=1.0
+            j=0.0   
+        
+        #if cv2.waitKey() == 0xcb: #seta esquerda
+         #   ind+=1    
+        #if cv2.waitKey() == 0xcd: #seta direita
+           # ind-=1
 
+ 
+#
 cv2.destroyAllWindows()
 
 #(COMPLETO) Mostre imagens contidas somente em um diretório. (1 ponto)
-#(QUASE) Finalizar a aplicação somente quando o usuário teclar 'q' ou 'Q'. (1 ponto).
+#(COMPLETO?) Finalizar a aplicação somente quando o usuário teclar 'q' ou 'Q'. (1 ponto).
 #(COMPLETO) No diretório pode ter imagens de diferente tamanhos, mas na tela as imagens devem ser apresentadas na resolução 512 x 512 (Dica o cropping ou resize da imagem) (2 pontos).
 #(COMPLETO) Trocar de imagem a cada 5 segundos (1 ponto).
 #(COMPLETO) Colocar uma borda da cor branca na imagem apresentada de tamanho 10x10x10x40. A margem de tamanho 40 deve ficar no topo (1 ponto).
--- aula03/testes.py
+++ aula03/testes.py
@@ -1,25 +1,16 @@
 from cv2 import cv2 
 import numpy as np
 
-i=0.0
-j=1.0
-n = []
-
 img = cv2.imread('data/lena.jpg')
-img2 = cv2.imread("data/macaco.jpg")
+ok = True
 
-while True:
-    if (i==0):
-        for contador in range(10):           
-            i+=0.10
-            j-=0.10
-            n = cv2.addWeighted(img,i,img2,j,1)
-            cv2.imshow('Img',n)
-            cv2.waitKey(100)
-        i=1
-        j=0    
-        cv2.waitKey(1000)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break 
-    
-cv2.waitKey()
+while ok:
+    cv2.imshow('Img',img)
+        
+    if cv2.waitKey(1)  == 113:
+        ok = False
+    if cv2.waitKey(1) & 0xFF == ord('Q'):
+        ok = False
+#cv2.waitKey()
+
+cv2.destroyAllWindows()
