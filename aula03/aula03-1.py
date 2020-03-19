e68e877fd7b043b5d3bb39d4c7f6394aed6031a1
guigui <guilherme.gasp@hotmail.com>
2020-03-19 18:16:39 -0300

    adicionado transição, mas não fecha ao apertar q

--- aula03/aula03-1.py
+++ aula03/aula03-1.py
@@ -31,46 +31,37 @@ for img in glob.glob("data/*.jpg"):
     images.append(n)
 
 ind  = 0
-i=1
-j=0
-while True:       
-   img = images[ind]
-    for contador in range(11):           
-            i+=0.10
-            j-=0.10
-            n = cv2.addWeighted(img,i,img2,j,1)
+i=1.0
+j=0.0
+while True: 
+    n = images[ind]
+    cv2.imshow('Img',n)      
+    cv2.waitKey(5000)
+    if(ind<3): 
+        for contador in range(10):           
+            i-=0.10
+            j+=0.10
+            n = cv2.addWeighted(images[ind],i,images[ind+1],j,1)
             cv2.imshow('Img',n)
-            cv2.waitKey(100)
-        i=1
-        j=0    
-        cv2.waitKey(1000)
-   cv2.imshow("Fotos", img)
-   cv2.waitKey(5000)
-   cv2.addWeighted()
-   ind+=1
-   if cv2.waitKey(1) & 0xFF == ord('q'):
-    break 
-   if cv2.waitKey(1) & 0xFF == ord('Q'):
-    break     
-   #if i == 4:
-    #i=0   
-    #if cv2.waitKey(1) & 0x44 == ord('d'):
-     #  i+=1      
-    #if cv2.waitKey(1) & 0x41 == ('a'):
-     #  i-=1      
-    
-    
+            cv2.waitKey(100)   
+        ind+=1
+        i=1.0
+        j=0.0
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+    if cv2.waitKey(1) & 0xFF == ord('Q'):
+        break          
 
 cv2.waitKey()
 
 cv2.destroyAllWindows()
 
 #(COMPLETO) Mostre imagens contidas somente em um diretório. (1 ponto)
-#(COMPLETO) Finalizar a aplicação somente quando o usuário teclar 'q' ou 'Q'. (1 ponto).
+#(QUASE) Finalizar a aplicação somente quando o usuário teclar 'q' ou 'Q'. (1 ponto).
 #(COMPLETO) No diretório pode ter imagens de diferente tamanhos, mas na tela as imagens devem ser apresentadas na resolução 512 x 512 (Dica o cropping ou resize da imagem) (2 pontos).
 #(COMPLETO) Trocar de imagem a cada 5 segundos (1 ponto).
 #(COMPLETO) Colocar uma borda da cor branca na imagem apresentada de tamanho 10x10x10x40. A margem de tamanho 40 deve ficar no topo (1 ponto).
 #(COMPLETO) Escrever dentro da borda do topo o nome da imagem (1 ponto).
 #(COMPLETO) Colocar um marca d'água no canto inferior da imagem e fora da borda (1 ponto).
-#Aplique uma transição entre uma image e outra (Dica: veja a função  cv2.addWeighted) (1 ponto). 
+#(COMPLETO) Aplique uma transição entre uma image e outra (Dica: veja a função  cv2.addWeighted) (1 ponto). 
 #Ao teclar seta para direita ou esquerda, avançar para a próxima imagem ou retroceder para a imagem anterior (1 ponto)
--- aula03/testes.py
+++ aula03/testes.py
@@ -1,8 +1,8 @@
 from cv2 import cv2 
 import numpy as np
 
-i=1.0
-j=0.0
+i=0.0
+j=1.0
 n = []
 
 img = cv2.imread('data/lena.jpg')
@@ -10,7 +10,7 @@ img2 = cv2.imread("data/macaco.jpg")
 
 while True:
     if (i==0):
-        for contador in range(11):           
+        for contador in range(10):           
             i+=0.10
             j-=0.10
             n = cv2.addWeighted(img,i,img2,j,1)
@@ -20,18 +20,6 @@ while True:
         j=0    
         cv2.waitKey(1000)
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows() 
-    elif (i==1.0):
-        for contador2 in range(10):
-            j+=0.10
-            i-=0.10
-            n = cv2.addWeighted(img,i,img2,j,1)
-            cv2.imshow('Img',n)
-            cv2.waitKey(100)
-        i=0
-        j=1
-        cv2.waitKey(1000)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows() 
+            break 
     
 cv2.waitKey()
